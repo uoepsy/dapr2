217dd4c868391727ec5571cc304fe8f33ae7c501
epankratz <ecpankratz@gmail.com>
2026-07-16 09:41:51 +0100

    update course outline in all lec slides

---
+++ dapr2_lectures/dapr2_course_outline.R
@@ -0,0 +1,35 @@
+block1_name = "Introduction to linear models <br>(with Dr. Patrick Sturt)"
+block1_lecs = c(
+  'Introduction to linear regression',
+  'Interpreting linear models',
+  'Testing predictors and evaluating linear models',
+  'F-tests and model comparison',
+  'Practice analysis: Linear models'
+)
+
+block2_name = "Extending linear models <br>(with Dr. Elizabeth Pankratz)"
+block2_lecs = c(
+  'Categorical predictors and treatment coding',
+  'Sum coding',
+  'Assumptions and diagnostics',
+  'Bootstrapping and confidence intervals',
+  'Practice analysis: Categorical predictors (led by Patrick)'
+)
+
+block3_name = "Interactions <br>(with Dr. Elizabeth Pankratz)"
+block3_lecs = c(
+  'Mean-centering and numeric/categorical interactions',
+  'Numeric/numeric interactions',
+  'Categorical/categorical interactions',
+  'Testing simple effects and correcting for multiple comparisons',
+  'Practice analysis: Interactions (led by Patrick)'
+)
+
+block4_name = "Logistic regression <br>(with Dr. Elizabeth Pankratz)"
+block4_lecs = c(
+  'Probabilities and log-odds',
+  'Modelling binary outcomes with logistic regression',
+  'Interactions, assumptions, diagnostics, comparisons',
+  'Practice analysis: Logistic regression (led by Patrick)',
+  'Report feedback, exam prep, full-course Q&A'
+)
